de67443b5c2d415273255b262a6bfd82c0ccdb21
Subh0048 <subhamkumarsahu99@gmail.com>
2025-01-02 04:03:45 +0530

    update context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "./context/Authcontext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
